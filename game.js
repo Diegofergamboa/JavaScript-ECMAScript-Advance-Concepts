8f03de7dfdc6da0f7b18d4f2c1dc44bc4857f425
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-01-22 18:52:52 -0500

    knowing hoe to work with the principles of canva and, the window event

--- game.js
+++ game.js
@@ -1,2 +1,8 @@
 const canvas = document.getElementById('#game');
 const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame); 
+
+function startGame() {
+    console.log('Content loaded.')
+}
