da489fb2a81980a15649da519d620cb5a57e65b4
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-01-23 15:43:09 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,23 @@
-const canvas = document.getElementById('#game');
+const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame); 
+
+
 
 function startGame() {
+    let canvasSize = () => {
+        if (window.innerHeight > window.innerWidth) {
+            canvasSize = window.innerWidth * 0.8;
+        } else {
+            canvasSize = window.innerHeight * 0.8;
+        }
+        return canvasSize;
+    };
+    const result = canvasSize();
+    
+    canvas.setAttribute('width', result);
+    canvas.setAttribute('height', result);
     console.log('Content loaded.');
 }
+
+window.addEventListener('load', startGame); 
