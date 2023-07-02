46d880529f3d6db6b56cbe1cc62096540db4ac1f
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-07-01 22:17:07 -0500

    How the default params is working

---
+++ 03-default-params.js
@@ -0,0 +1,13 @@
+// Before ECMAScript update.
+// This is the default config name.
+function newUsers(name, age, country) {
+    var name = name || 'Default name'
+    var  age = age || 22
+    var country =  country  || 'Default country'
+}
+
+
+// This is the new syntaxis with the ECMAScript with the default values.
+function newAdmin(name = 'Default name', age = 23, country = 'USA') {
+    return (name, age, country)
+}
