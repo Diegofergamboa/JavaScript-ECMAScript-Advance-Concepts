d48961ccb8363b9b914181e06aa1b0944a3b08d5
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-07-01 20:56:21 -0500

    Why and how to use arrow functions

---
+++ 01-arrow-functions.js
@@ -0,0 +1,12 @@
+// normal function
+function square(num) {
+    return num * num
+}
+
+// Here we can see as we set as a default function the square function
+const square = (num) => {
+    return num * num
+}
+
+// You can just pass to the execution comparaing against the another concepts
+const square = num => num * num
