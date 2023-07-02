4d1920ba483d868f62ce501c3b96f8f512d976f5
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-07-01 21:29:03 -0500

    examples of how the template literals with the strings are amazing to work on it

---
+++ 02-string.js
@@ -0,0 +1,23 @@
+// The template literals allow to us have funcionalities with the strings and objects
+
+const hello = 'hello'
+const world = 'world'
+
+// Before, we have to concatenate with + or ','
+const phrase = hello + ' ' + world
+
+// Thats why here exists another friendly propose
+const literalPhrase = `${hello} ${world}`
+
+// multi - line strings. Before ECMAScript we have to set for example \n for a linea jump as the next example.
+
+const lorem = 'this is an example for the first line \n'
+const lorem2 = 'this is the second string after line jump'
+
+// With ECMAScript we can just made that jump over here.
+
+const templateString = `this is the first line
+    this is the second line
+`
+
+console.log(templateString)
