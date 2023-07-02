1679ef530a177ccf9dd5e6a4fae6ae8bbf69eba0
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-07-01 23:11:45 -0500

    rest spread is the way to use the assign from an array using destructuring or an object just in case

---
+++ 04-rest-spread.js
@@ -0,0 +1,19 @@
+// With the apply of ECMAScript, we can work with acces to an especific item from an object or array due to the case.
+
+function getFullName() {
+    // Obtaining the user data
+    return ["Diego", "Gamboa"];
+}
+
+// Using destructuring with arrays.
+let [firstName, lastName] = getFullName();
+console.log(firstName,);
+
+
+const users = {
+    username : 'Diego',
+    id: 123
+}
+const {username , id} =  users
+
+console.log(username, id)
