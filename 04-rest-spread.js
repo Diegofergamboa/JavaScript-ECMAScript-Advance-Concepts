230064bf8da169302e58dda0f59d39a6c76c5ea3
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-07-02 14:35:07 -0500

    spared operator and rest params

--- 04-rest-spread.js
+++ 04-rest-spread.js
@@ -15,3 +15,27 @@ const users = {
 }
 const {username , id} =  users
 
+// Spread operator.
+
+let person = {
+    nam: 'Diego',
+    age: 23
+}
+
+let country = 'CO'
+
+// We can allow to set from different arrays.
+// Where is in another location we can put it on this stage of the variable.
+
+let  data  = { ...person, country }
+
+// Rest params
+// This add all the values.
+
+function sum(num , ...values) {
+    console.log(values)
+    return num + values[0]
+}
+
+const result = sum(1, 2, 3, 4, 4, 5, 6, 7, 8)
+console.log(result)
