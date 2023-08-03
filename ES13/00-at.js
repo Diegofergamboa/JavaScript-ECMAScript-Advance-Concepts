4623c7ad70dc93abea42e5852c161ff023de8d3e
diegofergamboa <diegofergamboa@hotmail.es>
2023-08-03 18:44:23 -0500

    Testing ssh

--- ES13/00-at.js
+++ ES13/00-at.js
@@ -3,5 +3,5 @@ const array = ['one', 'two', 'three', 'four', 'five', 'six']
 // Showing the last element of the array
 console.log(array[array.length - 1])
 
-// With this feature we can acces to the last item.
+// With this feature we can acces to the last item
 console.log(array.at[1])
